262237c09cf024c44888a33f462b57b44fc795a4
Sonzzini <paulo.sonzzini@gmail.com>
2024-11-18 12:34:34 -0300

    Initial Commit

---
+++ Desafio_TechAcme/Desafio_TechAcmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Desafio_TechAcmeApp.swift
+//  Desafio_TechAcme
+//
+//  Created by Paulo Sonzzini Ribeiro de Souza on 18/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Desafio_TechAcmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Desafio_TechAcmeTests/Desafio_TechAcmeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Desafio_TechAcmeTests.swift
+//  Desafio_TechAcmeTests
+//
+//  Created by Paulo Sonzzini Ribeiro de Souza on 18/11/24.
+//
+
+import Testing
+@testable import Desafio_TechAcme
+
+struct Desafio_TechAcmeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
