f064fe415bd4e1dc3d4fa542d922df454fc31c03
Sonzzini <paulo.sonzzini@gmail.com>
2024-11-24 20:46:03 -0300

    atualizar os testes para a nova implementação

--- Desafio_TechAcmeTests/Desafio_TechAcmeTests.swift
+++ Desafio_TechAcmeTests/Desafio_TechAcmeTests.swift
@@ -12,13 +12,15 @@ struct Desafio_TechAcmeTests {
 	
 	@Test func RepositoryDecoding() async throws {
 		let dataSource = MockDataSource()
-		let result = try await dataSource.fetchRepository()
+		
+		let request = RepositoryWebAccess.Request(q: "", sort: "", page: nil, order: nil)
+		let result = try await dataSource.fetchRepository(request: request)
 		#expect(result.totalCount > 0 && result.items.count > 0)
 	}
 	
 	@Test func PullRequestDecoding() async throws {
 		let dataSource = MockDataSource()
-		let result = try await dataSource.fetchPullRequests(owner: "", repo: "")
+		let result = try await dataSource.fetchPullRequests(request: PullRequestWebAccess.Request(owner: "", repository: "", state: nil))
 		
 //		for item in result.items {
 //			print("\(item.user.login): \(item.title)")
